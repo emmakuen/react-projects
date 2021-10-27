ffd24c22da79c038fb7fd0914b3e558acad52e85
emmakuen <emmaku@protonmail.com>
2021-10-28 00:29:10 +0900

    add task-tracker project

---
+++ task-tracker/src/components/Footer.js
@@ -0,0 +1,12 @@
+import { Link } from 'react-router-dom'
+
+const Footer = () => {
+    return (
+        <footer>
+            <p>Copyright &copy; 2021</p>
+            <Link to="/about" className="link">About</Link>
+        </footer>
+    )
+}
+
+export default Footer
