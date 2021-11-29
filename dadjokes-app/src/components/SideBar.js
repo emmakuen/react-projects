41d072ba5f551b497d33af9869905565bb717a4c
emmakuen <emmaku@protonmail.com>
2021-11-29 09:16:06 +0900

    add notification message

--- dadjokes-app/src/components/SideBar.js
+++ dadjokes-app/src/components/SideBar.js
@@ -1,7 +1,25 @@
-import React from "react";
+import React, { useState } from "react";
 import "../styles/SideBar.css";
 
 const SideBar = ({ fetchJokes }) => {
+  const [showNotification, setShowNotification] = useState(false);
+  const [notification, setNotification] = useState("");
+
+  let notificationText = "";
+  const handleClick = () => {
+    setShowNotification(true);
+    setNotification("Fetching...");
+    fetchJokes().then((res, err) => {
+      if (!err) {
+        setNotification("Jokes added!");
+        setTimeout(() => {
+          setShowNotification(false);
+          setNotification("");
+        }, 1500);
+      }
+    });
+  };
+
   return (
     <div className="SideBar">
       <h1>
@@ -10,7 +28,10 @@ const SideBar = ({ fetchJokes }) => {
       <div>
         <p className="SideBar-emoji">😂</p>
       </div>
-      <button onClick={fetchJokes}>New Jokes</button>
+      <div className="button-container">
+        <button onClick={handleClick}>New Jokes</button>
+        {showNotification && <p className="notification">{notification}</p>}
+      </div>
     </div>
   );
 };
