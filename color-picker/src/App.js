19b76e35826a5de5e273a21d24d0b28b9420700b
emmakuen <emmaku@protonmail.com>
2021-12-03 00:05:06 +0900

    add react router

--- color-picker/src/App.js
+++ color-picker/src/App.js
@@ -1,12 +1,17 @@
-import "./styles/App.css";
+import { Route, Routes } from "react-router-dom";
 import Palette from "./components/Palette";
 import seedPalettes from "./utils/seedPalettes";
 import { generatePalette } from "./utils/colorHelper";
+import "./styles/App.css";
 
 function App() {
   return (
     <div className="App">
-      <Palette palette={generatePalette(seedPalettes[0])} />
+      <Routes>
+        <Route path="/" element={<h1>PALETTE LIST GOES HERE!</h1>} />
+        <Route path="/palette/:id" element={<h1>INDIVIDUAL PALETTE!</h1>} />
+      </Routes>
+      {/* <Palette palette={generatePalette(seedPalettes[0])} /> */}
     </div>
   );
 }
