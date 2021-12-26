02900d3a5b0509fada29ecdcd71ade425f39d78c
emmakuen <emmaku@protonmail.com>
2021-12-26 23:41:04 +0900

    add responsive style helper

---
+++ color-picker/src/styles/sizes.js
@@ -0,0 +1,30 @@
+// // Small devices (landscape phones, 576px and up)
+// @media (min-width: 576px) { ... }
+
+// // Medium devices (tablets, 768px and up)
+// @media (min-width: 768px) { ... }
+
+// // Large devices (desktops, 992px and up)
+// @media (min-width: 992px) { ... }
+
+// // X-Large devices (large desktops, 1200px and up)
+// @media (min-width: 1200px) { ... }
+
+// // XX-Large devices (larger desktops, 1400px and up)
+// @media (min-width: 1400px) { ... }
+
+const screenSizes = {
+  up() {},
+
+  down(size) {
+    const sizes = {
+      xs: "575.98px",
+      sm: "767.98px",
+      md: "991.98px",
+      lg: "1199.98px",
+    };
+    return `@media (max-width: ${sizes[size]})`;
+  },
+};
+
+export default screenSizes;
