83c3448a47bdf69029a30ed743f3dd6c78705f25
emmakuen <emmaku@protonmail.com>
2022-01-02 13:57:43 +0900

    change button color

--- color-picker/src/styles/NotFoundScreenStyles.js
+++ color-picker/src/styles/NotFoundScreenStyles.js
@@ -19,13 +19,13 @@ const NotFoundScreenStyles = {
     color: "white",
     fontSize: "1.5rem",
     fontWeight: 400,
-    backgroundColor: "rgba(255, 255, 255, 0.2)",
+    backgroundColor: "rgba(0, 0, 0, 0.2)",
     padding: "10px 20px",
     borderRadius: "10px",
     marginTop: "2rem",
     border: "#fff 1px solid",
     "&:hover": {
-      backgroundColor: "rgba(255, 255, 255, 0.3)",
+      backgroundColor: "rgba(0, 0, 0, 0.3)",
     },
   },
 };
