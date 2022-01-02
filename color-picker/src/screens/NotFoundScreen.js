11749e875a374ed23392055dc353bf5567d7a3e4
emmakuen <emmaku@protonmail.com>
2022-01-02 13:55:18 +0900

    style 404 screen

--- color-picker/src/screens/NotFoundScreen.js
+++ color-picker/src/screens/NotFoundScreen.js
@@ -1,7 +1,18 @@
 import React from "react";
+import { Link } from "react-router-dom";
+import { withStyles } from "@material-ui/styles";
+import NotFoundScreenStyles from "../styles/NotFoundScreenStyles";
 
-const NotFoundScreen = () => {
-  return <div>Not Found!</div>;
+const NotFoundScreen = ({ classes }) => {
+  return (
+    <div className={classes.root}>
+      <h1>404</h1>
+      <p>Page not found</p>
+      <Link to="/" className={classes.goBack}>
+        Home
+      </Link>
+    </div>
+  );
 };
 
-export default NotFoundScreen;
+export default withStyles(NotFoundScreenStyles)(NotFoundScreen);
---
+++ color-picker/src/styles/NotFoundScreenStyles.js
@@ -0,0 +1,33 @@
+import bg from "./bg.svg";
+
+const NotFoundScreenStyles = {
+  root: {
+    height: "100vh",
+    backgroundColor: "#e39a9d",
+    backgroundImage: `url(${bg})`,
+    display: "flex",
+    justifyContent: "center",
+    alignItems: "center",
+    flexDirection: "column",
+    color: "#fff",
+    fontSize: "2rem",
+    letterSpacing: "2px",
+    fontWeight: 700,
+  },
+  goBack: {
+    textDecoration: "none",
+    color: "white",
+    fontSize: "1.5rem",
+    fontWeight: 400,
+    backgroundColor: "rgba(255, 255, 255, 0.2)",
+    padding: "10px 20px",
+    borderRadius: "10px",
+    marginTop: "2rem",
+    border: "#fff 1px solid",
+    "&:hover": {
+      backgroundColor: "rgba(255, 255, 255, 0.3)",
+    },
+  },
+};
+
+export default NotFoundScreenStyles;
