fa2a69d0870e6863db84fbd4af6bfa837a36b847
emmakuen <emmaku@protonmail.com>
2022-04-03 12:43:54 +0900

    add react-dnd

--- bug-tracker/src/index.tsx
+++ bug-tracker/src/index.tsx
@@ -1,5 +1,7 @@
 import React from "react";
 import { createRoot } from "react-dom/client";
+import { DndProvider } from "react-dnd";
+import { HTML5Backend } from "react-dnd-html5-backend";
 import "./index.css";
 import App from "./App";
 import { AppStateProvider } from "./context/AppStateContext";
@@ -10,7 +12,9 @@ if (!rootElement) throw new Error("Failed to find the root element");
 const root = createRoot(rootElement);
 
 root.render(
-  <AppStateProvider>
-    <App />
-  </AppStateProvider>
+  <DndProvider backend={HTML5Backend}>
+    <AppStateProvider>
+      <App />
+    </AppStateProvider>
+  </DndProvider>
 );
